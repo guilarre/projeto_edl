b87a1250d16fedbaa9a9398763ed15c0e87fbab9
vinicius <valbuquerque950@gmail.com>
2026-05-19 18:18:37 -0300

    feat: funções de inserção, liberação e impreção da tabela hash

--- funcoes_hash.c
+++ funcoes_hash.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-#define TABLE_SIZE 11
+#define TABLE_SIZE 3
+
+//struct de cada user pra inserir na lista do array
+typedef struct No {
+    char email[100];
+    char senha_hash[100];
+
+    struct No *prox;
+} No;
 
 // Função pra transformar string em valor numérico
 unsigned int valor_str(char *str) {
@@ -35,17 +44,119 @@ void hash_criptografia(const char *senha, char *senha_hash) {
     sprintf(senha_hash, "%lx", hash); //transforma o hash (que é um long) em hex, dentro de senha_hash
 }
 
+void insere_hash(No *tabela[], char *email, char *senha_hash){
+    int indice = cria_hash(valor_str(email));
+
+    No *novo = (No *) malloc(sizeof(No));
+
+    strcpy(novo->email, email);
+    strcpy(novo->senha_hash, senha_hash);
+
+    novo->prox = tabela[indice];
+    tabela[indice] = novo;
+}
+
+void libera_hash(No *tabela[]){
+    for (int i = 0; i < TABLE_SIZE; i++){
+        No *atual = tabela[i];
+
+        while (atual != NULL){
+            No *temp = atual;
+
+            atual = atual->prox;
+
+            free(temp);
+        }
+        tabela[i] = NULL;
+    }
+}
+
+void imprime_tabela(No *tabela[]) {
+
+    printf("\n========== TABELA HASH ==========\n");
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+
+        printf("\nIndice [%d]\n", i);
+
+        No *atual = tabela[i];
+
+        // posição vazia
+        if (atual == NULL) {
+            printf("Vazio\n");
+            continue;
+        }
+
+        int colisao = 0;
+
+        while (atual != NULL) {
+
+            printf("Usuario: %s\n", atual->email);
+            printf("Hash da senha: %s\n", atual->senha_hash);
+
+            if (atual->prox != NULL) {
+                printf("COLISAO\n");
+                colisao++;
+            }
+
+            atual = atual->prox;
+        }
+
+        if (colisao > 0) {
+            printf("\nTotal de colisoes: %d\n", colisao);
+        }
+    }
+
+    printf("\n================================\n");
+}
+
 int main() {
-    // char senha_hasheada[30];
-    // hash_criptografia("Minhasupersenha1", senha_hasheada);
-    // printf("%s\n", senha_hasheada);
-    // hash_criptografia("Minhasupersenha2", senha_hasheada);
-    // printf("%s\n", senha_hasheada);
+    No *tabela[TABLE_SIZE];
+
+    //inicializa NULL em todas as posiçoes
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        tabela[i] = NULL;
+    }
+
+    // DEBUG====================================================================
+    char senha_hasheada[30];
+
+    hash_criptografia("Minhasupersenha1", senha_hasheada);
+    printf("senha hasheada: %s\n", senha_hasheada);
     int indice_hash = cria_hash(valor_str("guigaraposo2006@gmail.com"));
     printf("%d\n", indice_hash);
+    insere_hash(tabela, "guigaraposo2006@gmail.com", senha_hasheada);
 
+    printf("\n");
+
+    hash_criptografia("Minhasupersenha2", senha_hasheada);
+    printf("senha hasheada: %s\n", senha_hasheada);
     indice_hash = cria_hash(valor_str("guigaraposo2005@gmail.com"));
     printf("%d\n", indice_hash);
+    insere_hash(tabela, "guigaraposo2005@gmail.com", senha_hasheada);
+
+    printf("\n");
+
+    hash_criptografia("Minhasupersenha3", senha_hasheada);
+    printf("senha hasheada: %s\n", senha_hasheada);
+    indice_hash = cria_hash(valor_str("guigaraposo2026@gmail.com"));
+    printf("%d\n", indice_hash);
+    insere_hash(tabela, "guigaraposo2026@gmail.com", senha_hasheada);
+
+    printf("\n");
+
+    hash_criptografia("Minhasupersenha4", senha_hasheada);
+    printf("senha hasheada: %s\n", senha_hasheada);
+    indice_hash = cria_hash(valor_str("guigaraposo2010@gmail.com"));
+    printf("%d\n", indice_hash);
+    insere_hash(tabela, "guigaraposo2010@gmail.com", senha_hasheada);
+
+    printf("\n");
+    //==========================================================================
+
+    imprime_tabela(tabela);
+
+    libera_hash(tabela);
 
     return 0;
 }
